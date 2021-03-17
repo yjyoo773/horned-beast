5cff75e19d435ad599baf8ea0f209705cc8053c6
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-03-17 12:04:39 -0700

    minor fix

--- src/main.js
+++ src/main.js
@@ -6,16 +6,18 @@ class Main extends React.Component {
   render() {
     return (
       <div>
-        <ul>
-          {data.map((element) => (
-            <HornedBeast
-              title={element.title}
-              image_url={element.image_url}
-              description={element.description}
-              keyword={element.keyword}
-            />
+        {/* <ul> */}
+          {data.map((element,index) => (
+            <div key={index}>
+              <HornedBeast
+                title={element.title}
+                image_url={element.image_url}
+                description={element.description}
+                keyword={element.keyword}
+              />
+            </div>
           ))}
-        </ul>
+        {/* </ul> */}
       </div>
     );
   }
